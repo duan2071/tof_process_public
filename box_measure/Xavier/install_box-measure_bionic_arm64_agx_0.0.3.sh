4aa4133a0d1def146e98198c8db82a8932f46e64
Stefan Sobol <stefan.sobol@gmail.com>
2021-03-30 21:34:10 +0300

    Update release binary for Box v0.0.3

--- box_measure/Xavier/install_box-measure_bionic_arm64_agx_0.0.3.sh
+++ box_measure/Xavier/install_box-measure_bionic_arm64_agx_0.0.3.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3791393160e8d42f526a99e2a42feff00e7a98e059e54fdc55cf102fccd77de5
-size 14735364
+oid sha256:e904a7968942053834d830bc7b6621355f869622a37abce0b2ce10adca8a9041
+size 14737604
