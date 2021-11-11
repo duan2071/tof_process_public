286108421309d7c66726a40060c527d33e0ef4d0
Stefan Sobol <stefan.sobol@gmail.com>
2021-11-11 11:33:51 +0200

    Update door-sense and install script

--- adi_smart_camera/install_bionic_nano.sh
+++ adi_smart_camera/install_bionic_nano.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8740d7c597fef33f8235fe6c38ec162b5a887c3c4137fb2c1db214c038731ba8
-size 660
+oid sha256:d91a07ea4bfe44a29a0dad766fe7fd024dec06341c4f94b7f7dfed5d1bf3969d
+size 754
