253a32fa207f7387109083406dec1eb97f9249ec
Stefan Sobol <stefan.sobol@gmail.com>
2021-11-12 12:21:08 +0200

    Update smart camera install script

--- adi_smart_camera/install_bionic_nano.sh
+++ adi_smart_camera/install_bionic_nano.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d91a07ea4bfe44a29a0dad766fe7fd024dec06341c4f94b7f7dfed5d1bf3969d
-size 754
+oid sha256:f6d78bba747e4b8bfbf1dbc66901c8f0d8a737714f6cba7dc9887122ffcd5b23
+size 1396
